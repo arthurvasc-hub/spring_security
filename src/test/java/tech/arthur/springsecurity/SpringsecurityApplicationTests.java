a9858525617e7d42b93f6deb1ae0369fb4c59955
arthurvasc <arthurvasc2@gmail.com>
2024-11-27 21:11:22 -0300

    First commit

---
+++ src/test/java/tech/arthur/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.arthur.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
